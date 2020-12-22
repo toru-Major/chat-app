ff90c363af200c5e81bacb89d8fae0d8cb5635ae
toru-Major <tohru7718@gmail.com>
2020-12-22 19:32:16 +0900

    チャットルーム作成機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,  :validatable
+  validates :name, presence: true
 
   has_many :room_users
   has_many :rooms, through: :room_users
-  
-
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-
 end
